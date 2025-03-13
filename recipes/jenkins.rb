4d9bb0edbc3b48ac72f2710cf871ef9835603818
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-03-13 16:23:27 -0700

    Check if nested attribute exists before fetching.

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -30,9 +30,9 @@ apt_update "jenkins" do
   action :nothing
 end
 
-java_opts =  node['jenkins']['master']['java_opts']
+if node.exist?('jenkins', 'master', 'java_opts')
 
-if java_opts
+  java_opts =  node['jenkins']['master']['java_opts']
   directory '/etc/systemd/system/jenkins.service.d'
   template '/etc/systemd/system/jenkins.service.d/override.conf' do
     source 'jenkins-service-override.conf.erb'
