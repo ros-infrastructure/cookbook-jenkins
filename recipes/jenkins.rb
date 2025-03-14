c7051b14d79436a65c4b9868fcf2376b4a0d11d8
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-03-14 09:13:03 -0700

    Add compatibility layer for sous-chefs/jenkins attribute.
    Rather than just continuing to use the attributes from
    sous-chefs/jenkins. Add a compatibility warning but use an attribute
    path that makes more sense for this new cookbook.

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -30,7 +30,29 @@ apt_update "jenkins" do
   action :nothing
 end
 
+# Migrate from attributes used by sous-chefs/jenkins
+# The expected attributes have been changed but in order to remain compatible
+# with configurations from the prior cookbook a warning is being added if the
+# old attributes are set and differ from the new ones.
 if node.exist?('jenkins', 'master', 'java_opts')
+  if !node.exist?('jenkins', 'jenkins_java_opts')
+      Chef::Log.warn(
+        "The attribute `node['jenkins']['master']['java_opts']` is now `node['jenkins']['jenkins_java_opts']`. " +
+        "Support for the previous attribute may be removed in a future release of this cookbook. " +
+        "Replacing the `node['jenkins']['master']['java_opts']` attribute with `node['jenkins']['jenkins_java_opts']` is recommended."
+      )
+      node.default['jenkins']['jenkins_java_opts'] = node['jenkins']['master']['java_opts']
+  elsif node['jenkins']['jenkins_java_opts'] != node['jenkins']['master']['java_opts']
+      Chef::Log.warn(
+        "Both `node['jenkins']['master']['java_opts']` and `node['jenkins']['jenkins_java_opts']` are defined but differ. " +
+        "The `node['jenkins']['jenkins_java_opts']` attribute will be used. " +
+        "Support for the previous attribute may be removed in a future release of this cookbook. " +
+        "Removing the `node['jenkins']['master']['java_opts']` attribute is recommended."
+      )
+  end
+end
+
+if node.exist?('jenkins', 'jenkins_java_opts')
   execute "systemctl-daemon-reload" do
     command "systemctl daemon-reload"
     action :nothing
@@ -40,7 +62,7 @@ if node.exist?('jenkins', 'master', 'java_opts')
     action :nothing
   end
 
-  java_opts =  node['jenkins']['master']['java_opts']
+  java_opts =  node['jenkins']['jenkins_java_opts']
   directory '/etc/systemd/system/jenkins.service.d'
   template '/etc/systemd/system/jenkins.service.d/override.conf' do
     source 'jenkins-service-override.conf.erb'
