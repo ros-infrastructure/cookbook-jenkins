205bae0b80de246cd21d2f2fb36c08bee4792dd0
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2026-07-28 17:32:53 -0300

    remove expectation that jenkins is up when converging the recipe --- The way it was set before there was an expectation for Jenkins to be running when this recipe runs.  Modify the recipe so it executes on changes to the state of the jenkins service (start, or restart) at the end of the client run.

--- recipes/cli.rb
+++ recipes/cli.rb
@@ -1,4 +1,8 @@
 remote_file '/usr/local/jenkins/jars/jenkins-cli.jar' do
   source 'http://localhost:8080/jnlpJars/jenkins-cli.jar'
-  action :create
+  action :nothing
+  subscribes :create, 'service[jenkins]'
+  # Add retry logic since it might take a bit for Jenkins to be operational
+  retries 3
+  retry_delay 5
 end
