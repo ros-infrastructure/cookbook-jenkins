ccadbc1660ce9a8a9482f17ebc9313767eda6178
claraberendsen <42071084+claraberendsen@users.noreply.github.com>
2026-07-28 17:43:34 -0300

    ensure cli is present even after a reconverge --- If the recipe is executed in  reconverge it's possible that if the file was removed this resource never executes since there might not be an update to the jenkins status. Explicitely check this scenario and trigger the download.

--- recipes/cli.rb
+++ recipes/cli.rb
@@ -1,8 +1,18 @@
+# Jenkins must actually be serving requests for this to succeed. If it's
+# already running (e.g. a re-converge), download right away; otherwise wait
+# for service[jenkins] to (re)start it before trying.
+jenkins_running = system('systemctl is-active --quiet jenkins')
+
 remote_file '/usr/local/jenkins/jars/jenkins-cli.jar' do
   source 'http://localhost:8080/jnlpJars/jenkins-cli.jar'
-  action :nothing
-  subscribes :create, 'service[jenkins]'
   # Add retry logic since it might take a bit for Jenkins to be operational
   retries 3
   retry_delay 5
+
+  if jenkins_running
+    action :create_if_missing
+  else
+    action :nothing
+    subscribes :create, 'service[jenkins]', :delayed
+  end
 end
